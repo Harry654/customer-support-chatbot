ad50bb07bf1e079156ff381af68206bdf2510594
Harrison Akunne <harrisonderick.65@gmail.com>
2024-08-12 15:26:40 +0100

    feat: added UI for login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,29 +1,79 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { signInWithPopup } from "firebase/auth";
-import { useAuth } from "@/context/AuthContext";
+// import { useAuth } from "@/context/AuthContext";
 import { auth, googleProvider } from "@/lib/firebase/config";
+import { useRouter } from "next/navigation";
 
 const GoogleLogin = () => {
-  const { user } = useAuth();
+//   const { user } = useAuth();
+  const router = useRouter();
 
   const handleGoogleSignIn = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
+      router.push("/");
     } catch (error) {
       console.error("Google Sign-In Error:", error);
     }
   };
 
-  if (user) {
-    return <div>Welcome, {user.displayName}</div>;
-  }
+  //   useEffect(() => {
+  //     if (user) router.push("/");
+  //     else alert("Google Sign-In is currently the only availabe login option");
+  //   }, [user]);
 
+  //   if (user) {
+  //     return <div>Welcome, {user.displayName}</div>;
+  //   }
   return (
-    <div>
-      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
-      
+    <div id="login_box" className="w-screen min-h-screen bg-black">
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+      <form className="border">
+        <h3>Login Here</h3>
+
+        <label htmlFor="username" className="opacity-30">
+          Username
+        </label>
+        <input
+          type="text"
+          placeholder="Email or Phone"
+          id="username"
+          disabled={true}
+          className="opacity-30"
+        />
+
+        <label htmlFor="password" className="opacity-30">
+          Password
+        </label>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          disabled={true}
+          className="opacity-30"
+        />
+
+        <button
+          disabled={true}
+          className="bg-gray-50 opacity-30 mt-5 cursor-not-allowed"
+        >
+          Log In
+        </button>
+        <div className="social">
+          <button
+            className="bg-white mt-3"
+            onClick={handleGoogleSignIn}
+            type="button"
+          >
+            Google
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -264,27 +264,6 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="intro_video_bg">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="intro_video_iner text-center">
-                  <h2>Expect The Best</h2>
-                  <div className="intro_video_icon">
-                    <a
-                      id="play-video_1"
-                      className="video-play-button popup-youtube"
-                      href="https://www.youtube.com/watch?v=pBFQdxA-apI"
-                    >
-                      <span className="ti-control-play"></span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
         <section className="food_menu gray_bg">
           <div className="container">
             <div className="row justify-content-between">
@@ -1018,76 +997,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="review_part gray_bg section_padding">
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-5">
-                <div className="section_tittle">
-                  <p>Testimonials</p>
-                  <h2>Customers Feedback</h2>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-11">
-                <div className="client_review_part owl-carousel">
-                  <div className="client_review_single media">
-                    <div className="client_img align-self-center">
-                      <img src="/img/client/client_1.png" alt="" />
-                    </div>
-                    <div className="client_review_text media-body">
-                      <p>
-                        Also made from. Give may saying meat there from heaven
-                        it lights face had is gathered god dea earth light for
-                        life may itself shall whales made they're blessed whales
-                        also made from give may saying meat. There from heaven
-                        it lights face had amazing place
-                      </p>
-                      <h4>
-                        Mosan Cameron, <span>Executive of fedex</span>
-                      </h4>
-                    </div>
-                  </div>
-                  <div className="client_review_single media">
-                    <div className="client_img align-self-center">
-                      <img src="/img/client/client_1.png " alt="" />
-                    </div>
-                    <div className="client_review_text media-body">
-                      <p>
-                        Also made from. Give may saying meat there from heaven
-                        it lights face had is gathered god dea earth light for
-                        life may itself shall whales made they're blessed whales
-                        also made from give may saying meat. There from heaven
-                        it lights face had amazing place
-                      </p>
-                      <h4>
-                        Mosan Cameron, <span>Executive of fedex</span>
-                      </h4>
-                    </div>
-                  </div>
-                  <div className="client_review_single media">
-                    <div className="client_img align-self-center">
-                      <img src="/img/client/client_1.png" alt="" />
-                    </div>
-                    <div className="client_review_text media-body">
-                      <p>
-                        Also made from. Give may saying meat there from heaven
-                        it lights face had is gathered god dea earth light for
-                        life may itself shall whales made they're blessed whales
-                        also made from give may saying meat. There from heaven
-                        it lights face had amazing place
-                      </p>
-                      <h4>
-                        Mosan Cameron, <span>Executive of fedex</span>
-                      </h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+  
         <section className="blog_item_section blog_section section_padding">
           <div className="container">
             <div className="row">
